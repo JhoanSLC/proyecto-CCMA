734c0d10df282483b3956f1c061383601f1ac73e
Alejo0507 <castillocazorla05@gmail.com>
2024-05-19 15:57:43 -0500

    feat: :sparkles: ALmacenamiento de valores

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,248 @@
+import './animacion-secciones';
+import { SECTIONS as sect, BUTTONS as b } from './constantes';
+
+let user = {
+    "valor": 0,
+    "configuraciones": []
+};
+
+const addValor = (valor) => {
+    user["valor"] += valor;
+    console.log(user);
+};
+
+const addConfig = (config) => {
+    user["configuraciones"].push(config);
+    console.log(user["configuraciones"]);
+};
+
+b.sec1CalidadOptima.addEventListener("click", () => {
+    addValor(6000000);
+    sections();
+});
+
+b.sec1CalidadPrecio.addEventListener("click", () => {
+    addValor(3000000);
+    sections();
+});
+
+b.sec1NoImporta.addEventListener("click", () => {
+    addValor(0);
+    sections();
+});
+
+const sections = () => {
+    if (user["valor"] == 6000000) {
+        b.sec2Android.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("Android");
+        });
+        b.sec2AndroidIOS.addEventListener("click", () => {
+            addValor(9000000);
+            addConfig("Android & IOS");
+        });
+        b.sec2Escritorio.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("Escritorio");
+        });
+        b.sec2Windows.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("Windows");
+        });
+        b.sec2iOS.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("IOS");
+        });
+        b.sec3NoNecesita.addEventListener("click", () => {
+            addValor(0);
+            addConfig("No necesita");
+        });
+        b.sec3Sencilla.addEventListener("click", () => {
+            addValor(6000000);
+            addConfig("Sencilla");
+        });
+        b.sec3Personalizada.addEventListener("click", () => {
+            addValor(18000000);
+            addConfig("Personalizada");
+        });
+        b.sec3RepWeb.addEventListener("click", () => {
+            addValor(12000000);
+            addConfig("Replica Web");
+        });
+        b.sec4Gratis.addEventListener("click", () => {
+            addValor(750000);
+            addConfig("Gratis");
+        });
+        b.sec4Pago.addEventListener("click", () => {
+            addValor(750000);
+            addConfig("Pago");
+        });
+        b.sec4InApp.addEventListener("click", () => {
+            addValor(6000000);
+            addConfig("In-App");
+        });
+        b.sec4Otros.addEventListener("click", () => {
+            addValor(1500000);
+            addConfig("Otros");
+        });
+        b.sec5Redes.addEventListener("click", () => {
+            addValor(6000000);
+            addConfig("Redes Sociales");
+        });
+        b.sec5Email.addEventListener("click", () => {
+            addValor(3750000);
+            addConfig("Email");
+        });
+        b.sec5No.addEventListener("click", () => {
+            addValor(0);
+            addConfig("No");
+        });
+        b.sec5NoSe.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("No Sé");
+        });
+    } else if (user["valor"] == 3000000) {
+        b.sec2Android.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("Android");
+        });
+        b.sec2AndroidIOS.addEventListener("click", () => {
+            addValor(7000000);
+            addConfig("Android & IOS");
+        });
+        b.sec2Escritorio.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("Escritorio");
+        });
+        b.sec2Windows.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("Windows");
+        });
+        b.sec2iOS.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("IOS");
+        });
+        b.sec3NoNecesita.addEventListener("click", () => {
+            addValor(0);
+            addConfig("No necesita");
+        });
+        b.sec3Sencilla.addEventListener("click", () => {
+            addValor(4000000);
+            addConfig("Sencilla");
+        });
+        b.sec3Personalizada.addEventListener("click", () => {
+            addValor(12000000);
+            addConfig("Personalizada");
+        });
+        b.sec3RepWeb.addEventListener("click", () => {
+            addValor(8000000);
+            addConfig("Replica Web");
+        });
+        b.sec4Gratis.addEventListener("click", () => {
+            addValor(500000);
+            addConfig("Gratis");
+        });
+        b.sec4Pago.addEventListener("click", () => {
+            addValor(500000);
+            addConfig("Pago");
+        });
+        b.sec4InApp.addEventListener("click", () => {
+            addValor(4000000);
+            addConfig("In-App");
+        });
+        b.sec4Otros.addEventListener("click", () => {
+            addValor(1000000);
+            addConfig("Otros");
+        });
+        b.sec5Redes.addEventListener("click", () => {
+            addValor(4000000);
+            addConfig("Redes Sociales");
+        });
+        b.sec5Email.addEventListener("click", () => {
+            addValor(2500000);
+            addConfig("Email");
+        });
+        b.sec5No.addEventListener("click", () => {
+            addValor(0);
+            addConfig("No");
+        });
+        b.sec5NoSe.addEventListener("click", () => {
+            addValor(2000000);
+            addConfig("No Sé");
+        });
+    } else {
+        b.sec2Android.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("Android");
+        });
+        b.sec2AndroidIOS.addEventListener("click", () => {
+            addValor(5000000);
+            addConfig("Android & IOS");
+        });
+        b.sec2Escritorio.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("Escritorio");
+        });
+        b.sec2Windows.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("Windows");
+        });
+        b.sec2iOS.addEventListener("click", () => {
+            addValor(3000000);
+            addConfig("IOS");
+        });
+        b.sec3NoNecesita.addEventListener("click", () => {
+            addValor(0);
+            addConfig("No necesita");
+        });
+        b.sec3Sencilla.addEventListener("click", () => {
+            addValor(2000000);
+            addConfig("Sencilla");
+        });
+        b.sec3Personalizada.addEventListener("click", () => {
+            addValor(6000000);
+            addConfig("Personalizada");
+        });
+        b.sec3RepWeb.addEventListener("click", () => {
+            addValor(4000000);
+            addConfig("Replica Web");
+        });
+        b.sec4Gratis.addEventListener("click", () => {
+            addValor(250000);
+            addConfig("Gratis");
+        });
+        b.sec4Pago.addEventListener("click", () => {
+            addValor(250000);
+            addConfig("Pago");
+        });
+        b.sec4InApp.addEventListener("click", () => {
+            addValor(2000000);
+            addConfig("In-App");
+        });
+        b.sec4Otros.addEventListener("click", () => {
+            addValor(500000);
+            addConfig("Otros");
+        });
+        b.sec5Redes.addEventListener("click", () => {
+            addValor(2000000);
+            addConfig("Redes Sociales");
+        });
+        b.sec5Email.addEventListener("click", () => {
+            addValor(1250000);
+            addConfig("Email");
+        });
+        b.sec5No.addEventListener("click", () => {
+            addValor(0);
+            addConfig("No");
+        });
+        b.sec5NoSe.addEventListener("click", () => {
+            addValor(1000000);
+            addConfig("No Sé");
+        });
+    }
+};
+
+console.log(user);
 import './animacion-secciones'
 import { SECTIONS as sect, BUTTONS as b} from './constantes'
 import './regresar'
